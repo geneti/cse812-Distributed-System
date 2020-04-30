65399f1c6335fa98c7d049dae0587b24bfd45e9e
Zhongzheng Chen <chenzho7@msu.edu>
2020-04-30 14:17:16 -0400

    base3

--- graph/Base.py
+++ graph/Base.py
@@ -48,11 +48,11 @@ def base_channel_assignment(Nodes, Links, C_Links, argv, fni_all=False):
 	# calculate rank for each link
 	for i in range(len(Links)):
 		link_neighbours = len(Links[i].node1.out_neighbours) + len(
-			Links[i].node2.in_neighbours)
+		Links[i].node2.in_neighbours)
 		link_min_hop_count = min(Links[i].node1.min_hop_count, Links[i].node2.min_hop_count)
 		link_distance = Links[i].distance
 		Links[i].rank = link_neighbours * link_distance**2 * Links[i].node2.Rx_th / (link_min_hop_count * \
-			Links[i].node1.pt * Links[i].node1.gain * Links[i].node2.gain)
+		Links[i].node1.pt * Links[i].node1.gain * Links[i].node2.gain)
 	print('rank list generated of length: ', len(Links))
 
 	# create deep copy of links list and sort in descending order by rank
@@ -101,20 +101,24 @@ def base_channel_assignment(Nodes, Links, C_Links, argv, fni_all=False):
 					# score the current possible channel
 					# assume the busy idle ratio for 2 links is the average
 					bil = (des_links_list[j].busy_idle_ratio + des_links_list[it].busy_idle_ratio)/2
+					IRange = interference_ceiling
 					if delta_omega < 5:
-						Score[omega] += 0
-					elif Min_dis <= NIR:
-						Score[omega] += NIR/Min_dis * bil
+						IRange += 0
+					elif Min_dis <= NIR and Min_dis != 0:
+						IRange += NIR / Min_dis * bil
 					else:
-						Score[omega] += interference_ceiling
-								
+						IRange += interference_ceiling
+
+					if IRange > Min_dis:
+						Score[omega] += 1
+
 
 		# call set_channel function
 		des_links_list[it].set_channel_base(Score)
 
 		if argv.plot_steps:
-			fig_path = os.path.join(argv.fig_root, f"n{len(Nodes)}",
-									f"step_{it:04d}.png")
+			fig_path = os.path.join(argv.fig_root, f"base_n{len(Nodes)}",
+				f"step_{it:04d}.png")
 			plot_graph.plot_graph(Nodes, Links, fig_path=fig_path)
 			print(f'Saving to {fig_path} ')
 
@@ -138,7 +142,7 @@ def test_base_method(argv):
 		Ns = [0]
 	elif argv.plot_steps and len(Ns) > 1:
 		raise Exception(
-			"min_node and max_node should equal if you want to plot steps")
+		"min_node and max_node should equal if you want to plot steps")
 
 	for num in Ns:
 		# generate graph
@@ -149,7 +153,7 @@ def test_base_method(argv):
 			LM = pd.DataFrame([[20, 20], [20, 10], [10, 20], [10, 10]])
 		else:
 			t = NodeDistribution.location_matrix(argv.width, argv.height, num,
-												'Random')
+					'Random')
 			LM = t.generate()
 		# LM = pd.DataFrame([[20, 20], [20, 10], [10, 20], [10, 10]])
 		Nodes, Links = utils.gen_graph(LM, argv.gateway_prob, argv.path_loss)
@@ -160,13 +164,13 @@ def test_base_method(argv):
 
 		if len(Ns) == 1:
 			fni_list = base_channel_assignment(Nodes,
-											Links,
-											C_Links,
-											argv,
-											fni_all=True)
+					Links,
+					C_Links,
+					argv,
+					fni_all=True)
 		else:
 			fni_list_local = base_channel_assignment(Nodes, Links, C_Links,
-													argv)
+					argv)
 			fni_list.append(fni_list_local[-1])
 
 	fig, ax = plt.subplots()
